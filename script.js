5331f623e109579821632a4748b2e9b422b5c2ae
Andrei Rodel <andrei_rodel@hotmail.com>
2025-07-07 11:01:59 -0300

    finalização do projeto

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ function selecionarTipo(element) {
   document.querySelectorAll('.tipo button').forEach(btn => btn.classList.remove('active'));
   element.classList.add('active');
 }
+
+document.querySelectorAll('.cores button').forEach(btn => {
+  btn.addEventListener('click', function() {
+    document.querySelectorAll('.cores button').forEach(b => b.classList.remove('cor-selecionada'));
+    this.classList.add('cor-selecionada');
+  });
+});
+
+document.querySelectorAll('.tema-card').forEach(card => {
+  card.addEventListener('click', function() {
+    document.querySelectorAll('.tema-card').forEach(c => c.classList.remove('tema-selecionado'));
+    this.classList.add('tema-selecionado');
+  });
+});
